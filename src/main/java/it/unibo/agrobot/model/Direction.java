9929cfcb86b6924454b6b01dd5e2aedb87ce4849
Matteo Botteghi <matteo.botteghi6@studio.unibo.it>
2026-06-05 15:18:01 +0200

    creazione classe Directions
    creazione del enum direction la quale contiene le direzioni per il movimento del drone

---
+++ src/main/java/it/unibo/agrobot/model/Direction.java
@@ -0,0 +1,12 @@
+package it.unibo.agrobot.model;
+
+/**
+ * Rappresenta le quattro direzioni cardinali principali in cui il drone può muoversi.
+ * Saranno poi associate ai comandi della tastiera (es. WASD).
+ */
+public enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
